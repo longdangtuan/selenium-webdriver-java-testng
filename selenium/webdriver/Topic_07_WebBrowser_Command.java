f50aee522f4aca94b6321b57f8747a65bbdf90ce
longdangtuan <longdangtuan0706@gmail.com>
2024-02-04 09:04:46 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	selenium/webdriver/Topic_05_Selenium_Text.java
    #	selenium/webdriver/Topic_06_XPath_Css_Excercise.java

--- selenium/webdriver/Topic_07_WebBrowser_Command.java
+++ selenium/webdriver/Topic_07_WebBrowser_Command.java
@@ -1,16 +1,20 @@
 package webdriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.logging.LogType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_07_WebBrowser_Command {
+    //**: hay dùng
+    //*: ít dùng
     // Cac cau lenh de thao tac voi Browser: driver.
     WebDriver driver;
 
@@ -18,18 +22,18 @@ public class Topic_07_WebBrowser_Command {
     // element.
 WebElement element;
     @Test
-    public void Register_01_Emty_Data(){
-        driver = new FirefoxDriver();
+    public void Register_01_Emty_Data() throws MalformedURLException {
+        driver = new FirefoxDriver(); //**
         // Set truc tiep
-        driver.get("https://www.facebook.com/");
+        driver.get("https://www.facebook.com/");//**
         // khia bao bien
         // Bien chi dung duy nhat 1 lan thi khong nen tao
         String homePageUrl = "https://www.facebook.com/";
         driver.get(homePageUrl);
         // dong 1 tab
-        driver.close();
+        driver.close();//*
         //dong broswer
-        driver.quit();
+        driver.quit();//**
         //2 ham close, quit se bi anh huong boi timeout cua implicitWait
         //findElement/findElements
 
@@ -37,13 +41,80 @@ WebElement element;
         // Nó sẽ đi tìm loại By nào và trả về element neu được tìm thấy (WebElement)
         // không được tìm thấy: Fail - throw exception: NoSuchElementException
         // Trả ve 1 element - nếu như tìm thấy nhieu hon 1 thì cũng chỉ lấy 1 (thao tac với cái đầu tiên)
-        WebElement emailAddressTextbox = driver.findElement(By.id("email"));
+        WebElement emailAddressTextbox = driver.findElement(By.id("email"));//**
 
         //findElements
         // Nó sẽ đi tìm với loại By no và trả veef 1 danh sách element neu như được tifm thay ( List WebElement)
         // ko tìm thấy - ko bị fail - trả veef list rỗng (0 element0
-        List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@id='email']"));
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@id='email']"));//**
 
+        // Lấy dữ lieu de lam gi?
+        // Dùng  để lấy ra url của page hiện taji đang đứng
+        driver.getCurrentUrl();//*
+        // Dùng để lấy ra title cura page hiện tại
+        driver.getTitle();
+        // lấy ra page source (HTML/CSS/JS) của page hieejn tại (verify 1 cách tương doi)
+        driver.getPageSource();
+        // laasy ra id của cửa sổ/ tab hiện tại
+        driver.getWindowHandles();//*
+
+        // Cooklies - Framework
+        driver.manage().getCookies();//*
+
+        //Get ra log o dev tool
+        driver.manage().logs().get(LogType.DRIVER);//*
+
+        // Apply cho việc tìm element (findElement/ findElements)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));//**
+
+        // chowf cho page được load xong
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        // Inject 1 đoạn code JS vafo trong Browser/ Element
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+        // Chạy full màn hình
+        driver.manage().window().fullscreen();
+        driver.manage().window().maximize();//**
+
+        // Test Responsive (Resolution)
+        driver.manage().window().setSize(new Dimension(1366, 768));
+        driver.manage().window().getSize();
+
+        // Set cho browser o vi tro so voi do phan giai man hinh ( run tren man hinh co kich thuoc bao nhieu )
+        driver.manage().window().setPosition(new Point(12, 30));
+        driver.manage().window().getPosition();
+
+        // Điều huong trang web
+        driver.navigate().back();
+        driver.navigate().refresh();
+        driver.navigate().forward();
+
+        // Thao tác với history cua web page (back / forward)
+        driver.navigate().to("https://www.facebook.com/");
+        driver.navigate().to(new URL("https://www.facebook.com/"));
+        driver.get("https/www.facebook.com/");
+
+        // Alert/ Window (tab) / Frame (iFrame)
+        driver.switchTo().alert().accept();//*
+        driver.switchTo().alert().dismiss();//*
+        driver.switchTo().alert().getText();//*
+        driver.switchTo().alert().sendKeys("a");//*
+
+       // lấy ra id của cuwra so/ tab hiện tại
+        // Handle Window/ Tab
+        String homePageWindowID = driver.getWindowHandle();//*
+        driver.switchTo().window(homePageWindowID);//*
+
+        // Switch/ handle frame (iframe)
+        // Index/ ID (name)/ Element
+        driver.switchTo().frame(0);//*
+        driver.switchTo().frame("0");//*
+        driver.switchTo().frame(driver.findElement(By.id("a")));//*
+
+        // Switch ve HTML chứa frame trc đó
+        driver.switchTo().defaultContent();
+
+        // Từ frame trong đi ra frame ngoài
+        driver.switchTo().parentFrame();
     }
 
 
---
+++ selenium/webdriver/Topic_08_WebElement_Commands.java
@@ -0,0 +1,98 @@
+package webdriver;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Topic_08_WebElement_Commands {
+    WebDriver driver;
+
+    @Test
+    public void TC_01_Element(){
+        // HTML Element: Textbox/ TextArea/Dropdown/Checkbox/Link/Button...
+        // Tifm chưa tương tác lên
+        driver.findElement(By.id(""));
+
+        // Tìm và tương tác  lên
+        driver.findElement(By.id("")).click();
+        //  Dng để xóa dữ lệu trong 1 field cho phép nhập (editable)
+        // Thường được  sử dụng trước hàm sendKey để đảm bảo tính toàn vẹn của dữ liệu
+        driver.findElement(By.id("")).clear(); //*
+        // Dùng để nhập liệu vào các field
+        driver.findElement(By.id("")).sendKeys("a");//**
+        // Dùng để click ln element
+        driver.findElement(By.id("")).click();//**
+        // Tìm từ node cha vào node con
+        driver.findElement(By.id("")).findElement(By.name(""));
+
+        // Tìm  và lưu vào 1 biến WebElement (chưa tương tác)
+        // Khi có  dùng biến này ít nhất 2 lần trở lên
+        WebElement fullNameTextBox = driver.findElement(By.id(""));
+        fullNameTextBox.clear();
+        fullNameTextBox.getAttribute("");
+
+        // Trả về nhiều element khớp với điều kiện
+        List<WebElement> textboxes = driver.findElements(By.cssSelector("")); // Java Generic: ràng buộc kiểu dữ liệu
+
+        // Verify 1 checkbox/ radio/ dropdown đã được chọn hay chưa
+       Assert.assertTrue(driver.findElement(By.id("")).isSelected());//*
+
+       // Dùng để verify 1 element bất kì có hiển thị hay không
+        Assert.assertTrue(driver.findElement(By.id("")).isDisplayed());//**
+
+        // Dùng để verify 1 element có được thao tác lên hay không (không phải read-only)
+        Assert.assertTrue(driver.findElement(By.id("")).isEnabled());//*
+
+        // HTML Element
+        driver.findElement(By.id("")).getAttribute("title");//*
+
+        // Tab Accessibility/Properties trong Element
+        driver.findElement(By.id("")).getAccessibleName();//*
+        driver.findElement(By.id("")).getDomProperty("");//*
+        driver.findElement(By.id("")).getAttribute("");//*
+
+        // Tab Styles trong Element (font, color,...)
+        driver.findElement(By.id("")).getCssValue("");//*
+
+        //Vị trí của element so với độ phân giải màn hình
+        Point nameTextboxLocation = driver.findElement(By.id("")).getLocation();
+
+        //Location + Size
+        driver.findElement(By.id("")).getRect();
+
+        //Shadow element
+        driver.findElement(By.id("")).getShadowRoot();
+
+        // Chiều cao và rộng
+        Dimension adddressSize = driver.findElement(By.id("")).getSize();
+
+        // Dropdown (default/ custom)
+        Select select = new Select(driver.findElement(By.id("")));
+
+        // Từ id/ clas/ name/ css/ xpath có thể truy  ra ngược lại tagname HTML
+        driver.findElement(By.id("")).getTagName();
+
+        driver.findElement(By.id("")).getText();//**
+
+        // Chụp hình và lưu dưới dạng Byte hoặc Base64
+        driver.findElement(By.id("")).getScreenshotAs(OutputType.BASE64); // Dạng text //*
+        driver.findElement(By.id("")).getScreenshotAs(OutputType.FILE); // file trong ổ cứng //*
+        driver.findElement(By.id("")).getScreenshotAs(OutputType.BYTES); //*
+
+        //Form (element nào là thẻ form hoặc nằm trong thẻ form) -> hành vi giống phím enter
+        //Register/Login/Search
+        driver.findElement(By.id("")).submit();
+
+        // Java non Generic: không ràng buộc kiểu dữ liệu
+        ArrayList name = new ArrayList();
+        name.add("a");
+        name.add(16);
+
+
+
+    }
+}
