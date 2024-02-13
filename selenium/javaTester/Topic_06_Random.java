7879d2bcb1aa96231aca3886ce5fa2af13a52c98
longdangtuan <longdangtuan0706@gmail.com>
2024-02-13 22:54:47 +0700

    TextBox/TextArea TestCase 1

---
+++ selenium/javaTester/Topic_06_Random.java
@@ -0,0 +1,12 @@
+package javaTester;
+
+import java.util.Random;
+
+public class Topic_06_Random {
+    // Java Builtin - Lấy sẵn ra sử dụng
+    // Java Libraries - Thư viện do 1 cá nhân/ tổ chức tự viết
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+    }
+}
