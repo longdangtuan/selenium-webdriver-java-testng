2fdfb01b99c987095b96fb3632830bd1a4a9d0f3
longdangtuan <longdangtuan0706@gmail.com>
2024-03-20 21:46:10 +0700

    Iframe Frame

---
+++ selenium/webdriver/Topic_20_Shadow_DOM.java
@@ -0,0 +1,96 @@
+package webdriver;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_20_Shadow_DOM {
+    WebDriver driver;
+
+    WebDriverWait explicitWait;
+    Actions actions;
+    JavascriptExecutor javascriptExecutor;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+        actions = new Actions(driver);
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        javascriptExecutor = (JavascriptExecutor) driver;
+        // ngầm định: không rõ ràng cho 1 trạng thái cụ thể nào của element
+        // ngầm định cho việc tìm elements - Find Element(s)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+
+    }
+
+
+
+    @Test
+    public void TC_08_Shadow_DOM()  {
+   driver.get("https://automationfc.github.io/shadow-dom/");
+
+       // Shadow DOM 1
+        WebElement shadowHost = driver.findElement(By.cssSelector("div#shadow_host"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+
+        String someText = shadowRoot.findElement(By.cssSelector("span#shadow_content>span")).getText();
+        System.out.println(someText);
+        Assert.assertEquals(someText,"some text");
+
+        WebElement checkboxShadow = shadowRoot.findElement(By.cssSelector("input[type='checkbox']"));
+        Assert.assertFalse(checkboxShadow.isSelected());
+
+
+        // Shadow DOM 2
+        WebElement nestedShadowHost = shadowRoot.findElement(By.cssSelector("div#nested_shadow_host"));
+        SearchContext nestedShadowRoot = nestedShadowHost.getShadowRoot();
+
+        String nestedText = nestedShadowRoot.findElement(By.cssSelector("div#nested_shadow_content>div")).getText();
+        System.out.println(nestedText);
+        Assert.assertEquals(nestedText,"nested text");
+    }
+
+    @Test
+    public void TC_09_Shadow_DOM_Popup()  {
+        driver.get("https://shopee.vn");
+        sleepInSeconds(3);
+        WebElement shadowHost = driver.findElement(By.cssSelector("shopee-banner-popup-stateful"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+
+        if (shadowRoot.findElement(By.cssSelector("div.home-popup")).isDisplayed()){ // Step 2: Nếu hiển thị
+            System.out.println("Shadow DOM is displayed");
+            shadowRoot.findElement(By.cssSelector("div.shopee-popup__close-btn")).click();
+        }
+
+        // Step 2: Nếu không hiển thị
+        driver.findElement(By.cssSelector("div.shopee-searchbar-input")).sendKeys("iphone 15 Pro Max");
+        driver.findElement(By.cssSelector("button.shopee-searchbar__search-button")).click();
+
+
+
+    }
+
+
+    @AfterClass
+    public void afterClass(){driver.quit();}
+
+    public void sleepInSeconds(long timeInSecond){
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
---
+++ selenium/webdriver/Topic_21_Frame_Iframe.java
@@ -0,0 +1,102 @@
+package webdriver;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_21_Frame_Iframe {
+    WebDriver driver;
+
+    WebDriverWait explicitWait;
+    Actions actions;
+    JavascriptExecutor javascriptExecutor;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+        actions = new Actions(driver);
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        javascriptExecutor = (JavascriptExecutor) driver;
+        // ngầm định: không rõ ràng cho 1 trạng thái cụ thể nào của element
+        // ngầm định cho việc tìm elements - Find Element(s)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+
+    }
+
+
+
+    @Test
+    public void TC_10_Iframe_Kyna()  {
+        driver.get("https://skills.kynaenglish.vn/");
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div.face-content>iframe")));
+
+        // Verify follower
+        Assert.assertEquals(driver.findElement(By.xpath("//a[@title='Kyna.vn']/parent::div/following-sibling::div")).getText(),"178K followers");
+
+        // Click vào chat iframe
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("cs_chat_iframe");
+        driver.findElement(By.xpath("//body[@ng-app='csChatApp']")).click();
+        driver.findElement(By.cssSelector("input.input_name")).sendKeys("Yanma");
+
+        // Sendkey vói từ khóa excel vào click vào search icon
+        driver.switchTo().defaultContent();
+        driver.findElement(By.cssSelector("input#live-search-bar")).sendKeys("excel");
+        driver.findElement(By.cssSelector("button.search-button")).click();
+
+        // Verify chuyển page
+        Assert.assertTrue(driver.findElement(By.xpath("//a[@class='card-popup' and @href='/hoc-excel-tu-cong-viec-thuc-te']")).isDisplayed());
+    }
+
+    @Test
+    public void TC_11_IFrame_formsite()  {
+    driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+    driver.findElement(By.cssSelector("div#imageTemplateContainer>img")).click();
+    driver.switchTo().frame(driver.findElement(By.cssSelector("div#formTemplateContainer>iframe")));
+
+    // Nhập dữ liệu vào 3 fields và ấn submit
+        new Select(driver.findElement(By.name("RESULT_RadioButton-2"))).selectByVisibleText("Freshman");
+        new Select(driver.findElement(By.name("RESULT_RadioButton-3"))).selectByVisibleText("East Dorm");
+        WebElement radioBtn4_1 = driver.findElement(By.id("RESULT_RadioButton-4_1"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", radioBtn4_1);
+
+        driver.findElement(By.cssSelector("input.submit_button")).click();
+        sleepInSeconds(3);
+
+        // Click vào login button
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//a[@title='Log in']")).click();
+
+        //Verify error message
+        driver.findElement(By.cssSelector("button#login")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#message-error")).getText(),"Username and password are both required.");
+
+
+
+
+    }
+
+
+    @AfterClass
+    public void afterClass(){driver.quit();}
+
+    public void sleepInSeconds(long timeInSecond){
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
